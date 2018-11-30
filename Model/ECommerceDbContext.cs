86a0f6257ab02e649da85c428a2695caba94cf49
yassin ajaoun <ajaounyassin@gmail.com>
2018-11-30 13:49:59 +0100

    Ajout BDD

---
+++ Model/ECommerceDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext()
+        {
+        }
+
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext>options):base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;port3306;database=ecommercedb;uid=root;password=");
+            }
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Person>()
+            //    .HasMany(bc => bc.DirectedMovies)
+            //    .WithOne(x => x.Director)
+            //    ;
+        }
+    }
+}
