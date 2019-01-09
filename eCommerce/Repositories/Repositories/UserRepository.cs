7942dd92cfc602a352bf835997865b6c4b7b38ac
Kevin <boucaud.kevin@gmail.com>
2019-01-09 12:18:07 +0100

    Modification User (Repo/Controller/service)

---
+++ eCommerce/Repositories/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Repositories.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public ShoppingCartRepository(ECommerceDbContext context)
+        {
+            this._context = context;
+        }
+
+        //Create
+        User Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+        //Read
+        List<User> GetUserByFName(string UserFirstName);
+        List<User> GetUserByLName(string UserLastName);
+        List<User> GetUserByAddress(Address UserAddress);
+
+        //Update
+        User Update(Guid userId, User user);
+
+        //Delete
+        bool Delete(Guid id)
+        {
+            if (this._context.Users.Find(id) != null)
+            {
+                _context.Users.Remove(_context.Users.Find(id));
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ eCommerce/Services/UserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class UserService
+{
+
+    public User CreateUser(User user)
+    {
+        
+    }
+}
